70b1dab069096556a6af633ff208672658ebc4d0
jared <jareddonaldson@gmail.com>
2016-09-28 19:09:58 -0400

    use different login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import { GoogleLogin } from 'react-google-login-component';
+//import { GoogleLogin } from 'react-google-login-component';
+import GoogleLogin from 'react-google-login';
 import { CLIENT_ID } from '../constants/client.constants';
+import { Icon } from 'react-fa';
 
 export default class Login extends Component {
   responseGoogle(res) {
-    var id_token = res.getAuthResponse().id_token;
-
-    console.log('token: ', id_token );
-
-    // anything else you want to do (save to localStorage)...
+    console.log(res);
   }
 
-  render () {
+  render() {
+    var googleLoginStyles = {
+      textAlign: 'left',
+      background: '#ea4335',
+      border: 'none',
+      color: '#fff',
+      padding: '8px 12px',
+      borderRadius: 5
+    };
+
+    var spanStyles = {
+      display: 'inline-block',
+      textAlign: 'right',
+      width: 35
+    };
+
     return (
-      <div>
-        <GoogleLogin socialId={ CLIENT_ID }
-          class="ui button google-login"
-          scope="profile"
-          responseHandler={ this.responseGoogle }
-          buttonText="Google Login"/>
-      </div>
+      <GoogleLogin clientId={ CLIENT_ID }
+        buttonText="Login"
+        onSuccess={ this.responseGoogle }
+        onFailure={ this.responseGoogle }
+        style={ googleLoginStyles }>
+        <Icon name='google' />
+        <span style={ spanStyles }>Login</span>
+      </GoogleLogin>
     );
   }
 }

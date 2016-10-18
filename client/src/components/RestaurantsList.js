3647a9e46e341603b06bf9a031d59bb9ac2a0620
jared <jareddonaldson@gmail.com>
2016-10-17 20:08:27 -0400

    add RestaurantsList component

---
+++ client/src/components/RestaurantsList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const RestaurantsList = ({ restaurants }) => {
+  return (
+    <ul className="list-group">
+      {restaurants.map((restaurant, index) => (
+        <li className="list-group-item" key={index}>
+          <Link to={`/order/${restaurant.id}`}>{restaurant.name}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+RestaurantsList.propTypes = {
+  restaurants: React.PropTypes.array.isRequired
+}
+
+export default RestaurantsList
